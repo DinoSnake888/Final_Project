b0f4cee75044b48d90b9445f855b402f7d296eda
anarikadze-oss <ana.rikadze@ht-la.org>
2026-05-29 10:05:50 -0700

    New GUI branch with Basic JPanel setup for library

--- src/GUI.java
+++ src/GUI.java
@@ -1,4 +1,23 @@
 import javax.swing.*;
+import java.awt.*;
 
 class GUI {
+
+    JFrame frame;
+    JPanel panel;
+
+    public GUI(){
+
+        frame = new JFrame("Library Interface");
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(50,50,50,50));
+        panel.setLayout(new GridLayout(6,1));
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setSize(400,400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+
+    }
 }
