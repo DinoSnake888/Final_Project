e5214e5f799ab4762deb2ea9d26f41cf4e3182eb
rheanaandreiovici-alt <rheana.andreiovici@ht-la.org>
2026-06-01 10:34:00 -0700

    Added the create and update method but is full of errors

--- src/BookManagment.java
+++ src/BookManagment.java
@@ -1,3 +1,4 @@
+
 public class BookManagment {
 
     public BookManagment(){
@@ -7,7 +8,30 @@ public class BookManagment {
 
 //Start of all the book methods
     public void createBook(){
+        String bookTitle = title.getText();
+        String bookAuthor = author.getText();
+        String bookYear = year.getText();
 
+        if (bookTitle.equals("") || bookAuthor.equals("") || bookYear.equals("")) {
+            return;
+        }
+
+        try {
+
+            int yearAsNumber = Integer.Int(bookYear);
+
+            Book newBook = new Book(bookTitle, bookAuthor, bookNumber);
+
+            books.add(newBook);
+
+            title.setText("");
+            author.setText("");
+            year.setText("");
+
+        } catch (NumberFormatException error) {
+            System.out.println("Year is not a number.");
+
+        }
     }
 
     public void removeBook(){
@@ -15,8 +39,33 @@ public class BookManagment {
     }
 
     public void updateBook(){
+        if (newTitle.equals("") || newAuthor.equals("") || newYear.equals("")) {
+            System.out.println("Error: Everything must be completed to update.");
+            return;
+        }
 
+        Book bookToUpdate = null;
+        for (Book book : books) {
+            if (book.getTitle().equls(titleToFind)) {
+                bookToUpdate = book;
+                break;
+            }
+        }
+        if (bookToUpdate !=null) {
+            try {
+                int yearAsNumber = Integer.Int(newYear);
+                bookToUpdate.setTitle(newTitle);
+                bookToUpdate.setAuthor(newAuthor);
+                bookToUpdate.setYear(yearasNumber);
+                System.out.println("Book updated successfully.");
+            }  catch (NumberFormatException error) {
+                System.out.println("Error: Year must be a number.");
+            }
+        } else {
+            System.out.println("Error: Book with title "" + titleToFind + "" not found.");
+        }
     }
+
 
     public void sortAlphabetical(){
 
