fb8261a27c0827db16b3b8d3868eb296f613fb7c
DinoSnake888 <jamie.manalang@ht-la.org>
2026-06-03 14:17:58 -0700

    making temp branch since need to give up computers

--- src/BookManagment.java
+++ src/BookManagment.java
@@ -1,17 +1,25 @@
-public class BookManagment {
+import java.util.ArrayList;
 
+
+public class BookManagment {
+    ArrayList<Book> books = new ArrayList<Book>();
+
+//    public static void main(String[] args){
+//        Book m = new Book("", "", "", 1, false);
+//        books.add(m);
+//    }
     public BookManagment(){
 
     }
 
+    //Start of all the book methods
+    public void createBook(Book book){
 
-//Start of all the book methods
-    public void createBook(){
-
+       //books.add(book);
     }
 
-    public void removeBook(){
-
+    public void removeBook(Book book){
+        books.remove(book);
     }
 
     public void updateBook(){
@@ -22,4 +30,30 @@ public class BookManagment {
 
     }
 
+    public void checkOut(Book book){
+        if (!book.checked){
+            book.checked = true;
+            System.out.println("This book is now checked out");
+        }
+        else{
+            System.out.println("This book has already been checked out");
+        }
+
+    }
+
+    public void checkIn(Book book){
+        if (book.checked){
+            book.checked = false;
+            System.out.println("This book is now checked in");
+        }
+        else{
+            System.out.println("This book has already been checked in");
+        }
+
+    }
+
+    String accessCode = "meow";
+
+
 }
+
